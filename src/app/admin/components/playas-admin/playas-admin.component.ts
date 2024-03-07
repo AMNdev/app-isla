2b0a5052c42bf05ef71e07355e1831e13f4011b3
SuperClase <amarqueznovella@hotmail.com>
2024-03-07 15:36:57 +0100

    Playas Admin completo: UI tabla y form, validaciones

--- src/app/admin/components/playas-admin/playas-admin.component.ts
+++ src/app/admin/components/playas-admin/playas-admin.component.ts
@@ -1,8 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { PlayasService } from 'src/app/features/playas/playas.service';
 import { ModalService } from '../../shared/services/modal.service';
 import { Playas } from 'src/app/shared/interfaces/playas.interface';
-import { FormControl, FormGroup, FormGroupDirective } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  Validators,
+} from '@angular/forms';
+import { MatTable } from '@angular/material/table';
 
 @Component({
   selector: 'app-playas-admin',
@@ -10,17 +16,28 @@ import { FormControl, FormGroup, FormGroupDirective } from '@angular/forms';
   styleUrls: ['./playas-admin.component.css'],
 })
 export class PlayasAdminComponent implements OnInit {
+  @ViewChild(MatTable) table!: MatTable<Playas>;
   public playas!: Playas[];
 
-
   public newPlaya = new FormGroup({
-    descripcionNuevo:  new FormControl(''),
-    gMapsNuevo:  new FormControl(''),
-    idNuevo:  new FormControl(''),
-    imagenNuevo:  new FormControl(''),
-    nombreNuevo:  new FormControl(''),
+    descripcionNuevo: new FormControl(''),
+    gMapsNuevo: new FormControl(''),
+    idNuevo: new FormControl('', [
+      Validators.pattern(/^[A-Za-z0-9]+$/),
+      Validators.minLength(4),
+    ]),
+    imagenNuevo: new FormControl(''),
+    nombreNuevo: new FormControl(''),
   });
 
+  public displayedColumns = [
+    'id',
+    'nombre',
+    'descripcion',
+    'gMaps',
+    'imagen',
+    'actions',
+  ];
 
   constructor(private data: PlayasService, private modals: ModalService) {}
 
@@ -37,33 +54,29 @@ export class PlayasAdminComponent implements OnInit {
 
   setPlaya(f: FormGroupDirective) {
     // TODO: condicion!!
-    // if (condicion para proseguir) {
-    // }
-
     const formData = this.newPlaya.value;
-    const playaEnviar: Playas = {
-      descripcion: formData.descripcionNuevo!,
-      gMaps: formData.gMapsNuevo!,
-      id: formData.idNuevo!,
-      imagen: formData.imagenNuevo!,
-      nombre: formData.nombreNuevo!,
-    };
+    if (f.form.valid && this.checkUniqueId(formData.idNuevo!)) {
+      const playaEnviar: Playas = {
+        descripcion: formData.descripcionNuevo!,
+        gMaps: formData.gMapsNuevo!,
+        id: formData.idNuevo!,
+        imagen: formData.imagenNuevo!,
+        nombre: formData.nombreNuevo!,
+      };
 
-    this.data.setPlaya(playaEnviar).subscribe({
-      next: (resp) => {
-        this.playas.push(resp);
-        f.resetForm();
-        this.newPlaya.reset();
-
-        // TODO: this.table.renderRows();
-        
-        this.modals.openSnackBar(
-          `Playa añadida con éxito: ${resp.id} - ${resp.nombre}`
-        );
-      },
-      error: (err) => this.modals.openSnackBar(err),
-    });
-
+      this.data.setPlaya(playaEnviar).subscribe({
+        next: (resp) => {
+          this.playas.push(resp);
+          f.resetForm();
+          this.newPlaya.reset();
+          this.table.renderRows();
+          this.modals.openSnackBar(
+            `Playa añadida con éxito: ${resp.id} - ${resp.nombre}`
+          );
+        },
+        error: (err) => this.modals.openSnackBar(err),
+      });
+    }
   }
 
   deletePlaya(playa: Playas) {
@@ -77,13 +90,19 @@ export class PlayasAdminComponent implements OnInit {
             next: () => {
               this.modals.openSnackBar('Playa eliminada correctamente');
               this.playas = this.playas.filter((x) => x.id != playa.id);
-              // TODO: asegurar que la playa desaparece de pantalla!
             },
-            error: ({message}) => this.modals.openSnackBar(message),
+            error: ({ message }) => this.modals.openSnackBar(message),
           });
         }
       });
   }
 
-
+  checkUniqueId(id: string): boolean {
+    const existePlaya = this.playas.find((playa) => playa.id == id);
+    if (!existePlaya) return true;
+    else {
+      this.modals.openSnackBar('Debe elegir otro identificador');
+      return false;
+    }
+  }
 }
